fed80e50748b3afa23fc4522ac626a0997aa7f5a
Yushadha Bogoda <yushadha@gmail.com>
2015-02-13 11:08:59 -0800

    create ticket works

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,11 +1,20 @@
 class TicketsController < ApplicationController
+  
   def index
+    @tickets_new = Ticket.new
+    @ticket = Ticket.all
   end
 
   def new
   end
 
   def create
+    @ticket = Ticket.new(ticket_params)
+    if @ticket.save
+      redirect_to tickets_path
+    else
+      render :new
+    end
   end
 
   def show
@@ -19,4 +28,14 @@ class TicketsController < ApplicationController
 
   def destroy
   end
+
+private
+  def ticket_params
+    params.require(:ticket).permit([:title, :description])
+  end
+
+  def find_ticket
+    @ticket = Ticket.find(params[:id])
+  end
+
 end
